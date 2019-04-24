854de644d5458dc8f2c08f5546fbb25632f08d87
Hayoung LEE <ch.jeju@gmail.com>
2019-04-24 10:40:06 +0000

    first commit

---
+++ 03_loo.py
@@ -0,0 +1,26 @@
+# n = 0
+#  while n < 5:
+#    print('대표님')    
+# foods = ['pizza']
+
+# for food in foods:
+#    print(food)
+ 
+    numbers = [1, 2, 3, 4, 5, 6]
+    #for number in numbers:
+     #   print (number=*2, end= '.')
+       
+        
+        new_numbers = []
+        for number in numbers:
+            new_numbers.append(number=*2)
+        
+        print(new_numbers)
+        
+        '''
+        n = n * 2
+        
+        dust = [12,13,14]
+        for i in dust: i=12
+        print(!)
+        '''
---
+++ 04_condition.py
@@ -0,0 +1,23 @@
+#if True:
+#    print('false')
+#else:
+#    print('true')
+dd
+dust = int(input('미세먼지 수치를 입력해주세요: '))
+
+if dust > 150 :
+    print ('나쁨입니')
+elif 80<dust<=150 :
+    print('maybe..')
+elif 30<dust<=80 :
+    print('maybe..')
+else:
+    print('짝수입니다')
+    
+#    def dust(data):   
+        
+ #       if 0 < int(data) < 40:
+  #          print ('종흠')
+  
+  
+        #추가미션 : 짝수는짝수 리스트에홀수는 홀수 리스트에출력 range(1,11
